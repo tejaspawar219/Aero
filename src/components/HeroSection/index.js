21192fad6eea59efc18b027392f7f70633fadb34
lancelot05 <ninadusatam@gmail.com>
2022-01-13 11:29:57 +0530

    added join us modal

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import  {Img,Summary,Text,Button, Container} from './HeroSection.styles.js'
 import IMG_URL from '../../assets/aircraft.svg'
 import {Wrapper,Content} from '../../pages/styles'
+import { Modal } from 'react-bootstrap';
+import Register from '../Register/index.js';
 
 function HeroSection(props) {
+     const [show, setShow] = useState(false);
+
+     const handleClose = () => setShow(false);
+     const handleShow = () => setShow(true);
      return (
           <Wrapper>
                <Container reverse={false}>
@@ -11,7 +17,9 @@ function HeroSection(props) {
                <Summary>
                     <Content>
                          <Text>Aero-RGIT is one of the most versatile, passionate and hard-working team at RGIT striving for excellence. Our clubs promotes innovative ideas and skills by bringing together the brightest minds of the institute.</Text>
-                         <Button>Join Us</Button>
+                         <Button onClick={handleShow}>Join Us</Button>
+                         <Register show={show} handleClose={handleClose} />
+                         
                     </Content>
                </Summary>
                </Container>
